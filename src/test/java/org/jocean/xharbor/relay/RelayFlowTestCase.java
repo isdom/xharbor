2746cafe181406fd59ac18816d7cc62543cd8844
ming.ma <isdom.maming@gmail.com>
2015-03-06 22:31:06 +0800

    使用jMock重写RelayFlowTestCase.testRecvResp

--- src/test/java/org/jocean/xharbor/relay/RelayFlowTestCase.java
+++ src/test/java/org/jocean/xharbor/relay/RelayFlowTestCase.java
@@ -25,10 +25,11 @@ import org.jocean.event.api.FlowStateChangedListener;
 import org.jocean.event.core.FlowContainer;
 import org.jocean.http.HttpRequestTransformer;
 import org.jocean.httpclient.api.Guide;
-import org.jocean.httpclient.api.GuideBuilder;
-import org.jocean.httpclient.api.HttpClient;
 import org.jocean.httpclient.api.Guide.GuideReactor;
 import org.jocean.httpclient.api.Guide.Requirement;
+import org.jocean.httpclient.api.GuideBuilder;
+import org.jocean.httpclient.api.HttpClient;
+import org.jocean.httpclient.api.HttpClient.HttpReactor;
 import org.jocean.httpserver.ServerAgent.ServerTask;
 import org.jocean.idiom.ExectionLoop;
 import org.jocean.xharbor.api.Dispatcher;
@@ -282,16 +283,103 @@ public class RelayFlowTestCase {
 
 	@Test
 	public void testRecvResp() throws Exception {
-		final HttpRequest httpRequest = 
-				new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/test");
+		final HttpClient	httpclient = jmock.mock(HttpClient.class);
+//		final Guide 		guide = jmock.mock(Guide.class);
+		final GuideBuilder 	guideBuilder = jmock.mock(GuideBuilder.class);
+		final Target 		target = jmock.mock(Target.class);
+		final Dispatcher 	dispatcher = jmock.mock(Dispatcher.class);
+		final Router<HttpRequest, Dispatcher> router = 
+				(Router<HttpRequest, Dispatcher>)jmock.mock(Router.class);
+		final RelayMemo 	memo = jmock.mock(RelayMemo.class);
+		final RelayMemo.Builder	memoBuilder = jmock.mock(RelayMemo.Builder.class);
+		
+		final ChannelHandlerContext channelCtx = jmock.mock(ChannelHandlerContext.class);
+		
+		final Guide 		guide = new Guide() {
+			@Override
+			public void detach() throws Exception {
+			}
+			@Override
+			public <CTX> void obtainHttpClient(CTX ctx,
+					GuideReactor<CTX> reactor, Requirement requirement) {
+				try {
+					reactor.onHttpClientObtained(ctx, httpclient);
+				} catch (Exception e) {
+				}
+			}};
+		jmock.checking(new Expectations() {   
+	        {   
+	        	allowing(httpclient).sendHttpRequest(
+	        			with(anything()), 
+	        			with(any(HttpRequest.class)), 
+	        			with(any(HttpReactor.class)));
+	        	
+	        	allowing(httpclient).sendHttpContent(
+	        			with(any(HttpContent.class)));
+	        	
+//	        	allowing(guide).obtainHttpClient(
+//	        			with(anything()), 
+//	        			with(any(GuideReactor.class)), 
+//	        			with(any(Requirement.class)));
+//	        	
+//	        	allowing(guide).detach();
+	        	
+	        	allowing(guideBuilder).createHttpClientGuide();
+	            will(returnValue(guide));
+	            
+	        	allowing(target).getGuideBuilder();
+	            will(returnValue(guideBuilder));
+	            
+	        	allowing(target).serviceUri();
+	            will(returnValue(new URI("http://127.0.0.1")));
+	            
+	        	allowing(target).isNeedAuthorization(with(any(HttpRequest.class)));
+	            will(returnValue(false));
+	            
+	        	allowing(target).getHttpRequestTransformerOf(with(any(HttpRequest.class)));
+	            will(returnValue(null));
+	            
+	        	allowing(target).rewritePath(with(any(String.class)));
+	            will(returnValue("/test"));
+	            
+	        	allowing(dispatcher).dispatch();
+	            will(returnValue(target));
+	            
+	        	allowing(router).calculateRoute(with(any(HttpRequest.class)), 
+	        			with(any(org.jocean.xharbor.api.Router.Context.class)));
+	            will(returnValue(dispatcher));
+	            
+	            ignoring(memo).beginBizStep(with(equal(STEP.ROUTING)));
+	            ignoring(memo).beginBizStep(with(equal(STEP.OBTAINING_HTTPCLIENT)));
+	            ignoring(memo).beginBizStep(with(equal(STEP.TRANSFER_CONTENT)));
+	            oneOf(memo).beginBizStep(with(equal(STEP.RECV_RESP)));
+	            
+	            allowing(memo).endBizStep(with(any(STEP.class)), with(any(long.class)));
+	            
+//	            oneOf(memo).incBizResult(with(equal(RESULT.SOURCE_CANCELED)), with(any(long.class)));
+	            
+	            allowing(memoBuilder).build(with(any(Target.class)), (RoutingInfo)with(anything()));
+	            will(returnValue(memo));
+	            
+	            allowing(channelCtx).channel();
+	            will(returnValue(null));
+	            
+	            allowing(channelCtx).close();
+	        }   
+	    });
+		
+		// execute
 		final RelayFlow relay = new RelayFlow(router, memoBuilder, null)
-			.attach(null, httpRequest);
+			.attach(channelCtx, new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/test"));
 		final EventReceiver receiver =  engine.create("testcase", relay.INIT, relay);
 		
 		final ServerTask task = EventUtils.buildInterfaceAdapter(ServerTask.class, receiver);
 		task.onHttpContent(LastHttpContent.EMPTY_LAST_CONTENT);
 		
-		assertEquals(lastStep, RelayMemo.STEP.RECV_RESP);
+		// verify
+        jmock.assertIsSatisfied();
+//		
+//		assertEquals(lastStep, RelayMemo.STEP.RECV_RESP);
 	}
 
 	@Test
