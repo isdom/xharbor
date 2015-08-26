00f6e9239e6316d728b58b63570ddf6d3ce4b33f
ming.ma <isdom.maming@gmail.com>
2015-08-26 18:49:41 +0800

    using inside Spring's xml feature to simplify xharbor's config code

--- src/main/java/org/jocean/xharbor/booter/ZKMain.java
+++ src/main/java/org/jocean/xharbor/booter/ZKMain.java
@@ -3,21 +3,22 @@
  */
 package org.jocean.xharbor.booter;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 /**
  * @author isdom
  *
  */
 public class ZKMain {
-    
+    /**
+     * @param args
+     * @throws Exception 
+     */
     public static void main(String[] args) throws Exception {
-        
-        @SuppressWarnings({ "unused", "resource" })
-        final ApplicationContext ctx =
-                new ClassPathXmlApplicationContext(
-                        new String[]{"xharbor/zkxharbor.xml"});
+        @SuppressWarnings({ "resource", "unused" })
+        final AbstractApplicationContext ctx = 
+                new ClassPathXmlApplicationContext("unit/zkbooter.xml");
     }
-
 }
