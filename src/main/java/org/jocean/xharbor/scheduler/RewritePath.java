92d824c877bc49661ecb72fbd97ff375e0cd2c1a
ming.ma <isdom.maming@gmail.com>
2016-12-22 16:05:09 +0800

    refactor RewritePathScheduler --> RewritePath, and fix TestCase

---
+++ src/main/java/org/jocean/xharbor/scheduler/RewritePath.java
@@ -0,0 +1,73 @@
+package org.jocean.xharbor.scheduler;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jocean.http.server.HttpServerBuilder.HttpTrade;
+import org.jocean.http.util.RxNettys;
+import org.jocean.xharbor.api.TradeReactor;
+
+import io.netty.handler.codec.http.DefaultHttpRequest;
+import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpRequest;
+import rx.Observable;
+import rx.Single;
+import rx.functions.Func1;
+
+public class RewritePath implements TradeReactor {
+
+    public RewritePath(
+            final String pathPattern, 
+            final String replaceTo) {
+        this._pathPattern = safeCompilePattern(pathPattern);
+        this._replaceTo = replaceTo;
+    }
+    
+    private static Pattern safeCompilePattern(final String regex) {
+        return null != regex && !"".equals(regex) ? Pattern.compile(regex) : null;
+    }
+    
+    @Override
+    public Single<? extends InOut> react(final HttpTrade trade, final InOut io) {
+        return io.inbound().compose(RxNettys.asHttpRequest())
+            .map(new Func1<HttpRequest, InOut>() {
+                @Override
+                public InOut call(final HttpRequest req) {
+                    if (null == req) {
+                        return null;
+                    } else {
+                        final Matcher matcher = _pathPattern.matcher(req.uri());
+                        if ( matcher.find() ) {
+                            return new InOut() {
+                                @Override
+                                public Observable<? extends HttpObject> inbound() {
+                                    return Observable.<HttpObject>just(rewriteRequest(matcher, req))
+                                        .concatWith(io.inbound()
+                                                .flatMap(RxNettys.splitFullHttpMessage())
+                                                .skip(1));
+                                }
+                                @Override
+                                public Observable<? extends HttpObject> outbound() {
+                                    return io.outbound();
+                                }};
+                        } else {
+                            //  not handle this trade
+                            return null;
+                        }
+                    }
+                }})
+            .toSingle();
+    }
+
+    private HttpRequest rewriteRequest(
+            final Matcher matcher,
+            final HttpRequest req) {
+        final DefaultHttpRequest newreq = new DefaultHttpRequest(req.protocolVersion(), 
+                req.method(), req.uri(), true);
+        newreq.headers().set(req.headers());
+        return newreq.setUri(matcher.replaceFirst(_replaceTo));
+    }
+
+    private final Pattern _pathPattern;
+    private final String _replaceTo;
+}
--- src/test/java/org/jocean/xharbor/scheduler/RewritePathTestCase.java
+++ src/test/java/org/jocean/xharbor/scheduler/RewritePathTestCase.java
@@ -1,74 +1,112 @@
 package org.jocean.xharbor.scheduler;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.jocean.http.util.HttpMessageHolder;
+import org.jocean.http.util.Nettys;
 import org.jocean.http.util.RxNettys;
-import org.jocean.xharbor.api.HttpMessageTransformer;
-import org.jocean.xharbor.api.SessionContext;
+import org.jocean.xharbor.api.TradeReactor;
+import org.jocean.xharbor.api.TradeReactor.InOut;
 import org.junit.Test;
 
+import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpObject;
-import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpVersion;
 import rx.Observable;
-import rx.functions.Func1;
-import rx.observers.TestSubscriber;
 
 public class RewritePathTestCase {
 
+    public static final byte[] CONTENT = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd' };
+    
     @Test
-    public final void testRewritePathTransformerSuccess() {
-        final HttpMessageTransformer transformer = 
-                new RewritePathScheduler("/yjy_psm/fetchMetadata", "/yjy_common/fetchMetadata");
+    public final void testRewritePathSuccess() {
+        final TradeReactor reactor = 
+                new RewritePath("/yjy_psm/fetchMetadata", "/yjy_common/fetchMetadata");
         
-        final DefaultFullHttpRequest request = 
+        final DefaultFullHttpRequest orgreq = 
                 new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/yjy_psm/fetchMetadata");
         
-        final TestSubscriber<HttpObject> reqSubscriber = new TestSubscriber<>();
+        final InOut io = 
+            reactor.react(null, new InOut() {
+                @Override
+                public Observable<? extends HttpObject> inbound() {
+                    return Observable.<HttpObject>just(orgreq);
+                }
+                @Override
+                public Observable<? extends HttpObject> outbound() {
+                    return null;
+                }})
+            .toBlocking().value();
         
-        Observable.<HttpObject>just(request)
-            .flatMap(RxNettys.splitFullHttpMessage())
-            .compose(transformer)
-            .subscribe(reqSubscriber);
+        final HttpMessageHolder holder = new HttpMessageHolder(0);
+        io.inbound().compose(holder.assembleAndHold()).subscribe();
         
-        reqSubscriber.assertValueCount(2);
-        assertEquals("/yjy_common/fetchMetadata", 
-                ((HttpRequest)reqSubscriber.getOnNextEvents().get(0)).uri());
-        assertEquals("/yjy_psm/fetchMetadata", request.uri());
+        final FullHttpRequest rwreq = holder.httpMessageBuilder(RxNettys.BUILD_FULL_REQUEST).call();
+        
+        assertEquals("/yjy_psm/fetchMetadata", orgreq.uri());
+        assertEquals("/yjy_common/fetchMetadata", rwreq.uri());
     }
 
     @Test
-    public final void testRewritePathSchedulerSuccess() {
-        final RewritePathScheduler scheduler = 
-                new RewritePathScheduler("/yjy_psm/fetchMetadata", "/yjy_common/fetchMetadata");
+    public final void testNoNeedRewritePath() {
+        final TradeReactor reactor = 
+                new RewritePath("/yjy_psm/fetchMetadata", "/yjy_common/fetchMetadata");
         
-        final DefaultFullHttpRequest request = 
-                new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/yjy_psm/fetchMetadata");
+        final DefaultFullHttpRequest orgreq = 
+                new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/noNeedRewrite");
         
-        final TestSubscriber<HttpObject> reqSubscriber = new TestSubscriber<>();
-        final Observable<HttpObject> reqs = Observable.<HttpObject>just(request).flatMap(RxNettys.splitFullHttpMessage());
+        final InOut io = 
+            reactor.react(null, new InOut() {
+                @Override
+                public Observable<? extends HttpObject> inbound() {
+                    return Observable.<HttpObject>just(orgreq);
+                }
+                @Override
+                public Observable<? extends HttpObject> outbound() {
+                    return null;
+                }})
+            .toBlocking().value();
         
-        scheduler.schedule(new SessionContext() {
-            @Override
-            public Observable<HttpObject> inbound() {
-                return reqs;
-            }
-            @Override
-            public Observable<HttpObject> outbound() {
-                return null;
-            }})
-        .flatMapObservable(new Func1<HttpMessageTransformer, Observable<HttpObject>>() {
-            @Override
-            public Observable<HttpObject> call(final HttpMessageTransformer trans) {
-                return reqs.compose(trans);
-            }})
-        .subscribe(reqSubscriber);
+        assertNull(io);
+    }
+
+    @Test
+    public final void testRewritePathAndKeepRequestBody() throws IOException {
+        final TradeReactor reactor = 
+                new RewritePath("/yjy_psm/fetchMetadata", "/yjy_common/fetchMetadata");
         
-        reqSubscriber.assertValueCount(2);
-        assertEquals("/yjy_common/fetchMetadata", 
-                ((HttpRequest)reqSubscriber.getOnNextEvents().get(0)).uri());
-        assertEquals("/yjy_psm/fetchMetadata", request.uri());
+        final DefaultFullHttpRequest orgreq = 
+                new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/yjy_psm/fetchMetadata",
+                        Unpooled.wrappedBuffer(CONTENT));
+        
+        final InOut io = 
+            reactor.react(null, new InOut() {
+                @Override
+                public Observable<? extends HttpObject> inbound() {
+                    return Observable.<HttpObject>just(orgreq);
+                }
+                @Override
+                public Observable<? extends HttpObject> outbound() {
+                    return null;
+                }})
+            .toBlocking().value();
+        
+        final HttpMessageHolder holder = new HttpMessageHolder(0);
+        io.inbound().compose(holder.assembleAndHold()).subscribe();
+        
+        final FullHttpRequest rwreq = holder.httpMessageBuilder(RxNettys.BUILD_FULL_REQUEST).call();
+        
+        assertEquals("/yjy_psm/fetchMetadata", orgreq.uri());
+        assertEquals("/yjy_common/fetchMetadata", rwreq.uri());
+        
+        assertTrue(Arrays.equals(Nettys.dumpByteBufAsBytes(rwreq.content()), CONTENT));
     }
 }
