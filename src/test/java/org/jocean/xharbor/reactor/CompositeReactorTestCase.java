f3d5e31d1d45ed341a2de757ec7a412fbdc79282
ming.ma <isdom.maming@gmail.com>
2016-12-25 19:45:26 +0800

    补充 CompositeReactor 的测试用例

---
+++ src/test/java/org/jocean/xharbor/reactor/CompositeReactorTestCase.java
@@ -0,0 +1,79 @@
+package org.jocean.xharbor.reactor;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.jocean.http.server.HttpServerBuilder.HttpTrade;
+import org.jocean.idiom.Ordered;
+import org.jocean.xharbor.api.TradeReactor;
+import org.jocean.xharbor.api.TradeReactor.InOut;
+import org.junit.Test;
+
+import io.netty.handler.codec.http.HttpObject;
+import rx.Observable;
+import rx.Single;
+import rx.functions.Func2;
+
+public class CompositeReactorTestCase {
+    
+    class OrderedTradeReactor implements TradeReactor, Ordered {
+
+        OrderedTradeReactor(final int ordinal, final Func2<HttpTrade, InOut, Single<? extends InOut>> doReact) {
+            this._ordinal = ordinal;
+            this._doReact = doReact;
+        }
+        @Override
+        public int ordinal() {
+            return this._ordinal;
+        }
+        @Override
+        public Single<? extends InOut> react(final HttpTrade trade, final InOut io) {
+            return this._doReact.call(trade, io);
+        }
+        
+        private final int _ordinal;
+        private final Func2< HttpTrade, InOut, Single<? extends InOut>> _doReact;
+    }
+
+    @Test
+    public final void testCompositeReactor() {
+        final CompositeReactor cr = new CompositeReactor();
+        final AtomicBoolean tr1Reacted = new AtomicBoolean(false);
+        final AtomicBoolean tr2Reacted = new AtomicBoolean(false);
+        
+        cr.addReactor(new OrderedTradeReactor(1, new Func2<HttpTrade, InOut, Single<? extends InOut>>() {
+            @Override
+            public Single<? extends InOut> call(HttpTrade t1, InOut t2) {
+                tr1Reacted.set(true);
+                assertTrue(tr2Reacted.get());
+                return Single.just(null);
+            }}));
+        
+        cr.addReactor(new OrderedTradeReactor(2, new Func2<HttpTrade, InOut, Single<? extends InOut>>() {
+            @Override
+            public Single<? extends InOut> call(HttpTrade t1, InOut t2) {
+                tr2Reacted.set(true);
+                assertFalse(tr1Reacted.get());
+                return Single.just(null);
+            }}));
+        
+        final InOut io =
+        cr.react(null, new InOut() {
+            @Override
+            public Observable<? extends HttpObject> inbound() {
+                return null;
+            }
+            @Override
+            public Observable<? extends HttpObject> outbound() {
+                return null;
+            }})
+        .toBlocking().value();
+        
+        assertTrue(tr1Reacted.get());
+        assertTrue(tr2Reacted.get());
+        assertNull(io);
+    }
+}
