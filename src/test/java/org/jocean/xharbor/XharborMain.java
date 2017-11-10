3db8634ac324a5fed2b90e759fd270a0ef41a288
ming.ma <isdom.maming@gmail.com>
2017-11-10 17:12:33 +0800

    添加用于调试的 XharborMain 主入口

---
+++ src/test/java/org/jocean/xharbor/XharborMain.java
@@ -0,0 +1,11 @@
+package org.jocean.xharbor;
+
+import org.jocean.j2se.booter.LocalMain;
+
+public class XharborMain {
+
+    public static void main(String[] args) throws Exception {
+        LocalMain.main(args);
+    }
+
+}
