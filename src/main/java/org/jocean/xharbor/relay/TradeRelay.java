1ef6769a4276453a51447bb0dcb566c85273126e
ming.ma <isdom.maming@gmail.com>
2018-06-24 16:32:59 +0800

    1. 简化 TradeRelay 实现方式 2. 在 local.yaml 中增加 hystrix 导出和转发配置: http://127.0.0.1:8889/hystrix/hystrix.stream

--- src/main/java/org/jocean/xharbor/relay/TradeRelay.java
+++ src/main/java/org/jocean/xharbor/relay/TradeRelay.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.jocean.xharbor.relay;
 
@@ -9,8 +9,6 @@ import java.nio.channels.ClosedChannelException;
 import org.jocean.http.TransportException;
 import org.jocean.http.server.HttpServerBuilder.HttpTrade;
 import org.jocean.http.util.RxNettys;
-import org.jocean.idiom.BeanHolder;
-import org.jocean.idiom.BeanHolderAware;
 import org.jocean.idiom.DisposableWrapper;
 import org.jocean.idiom.DisposableWrapperUtil;
 import org.jocean.idiom.ExceptionUtils;
@@ -24,18 +22,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.netty.handler.codec.http.HttpObject;
-import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpVersion;
 import rx.Observable;
 import rx.Subscriber;
-import rx.functions.Action1;
 import rx.functions.Func1;
 
 /**
  * @author isdom
  *
  */
-public class TradeRelay extends Subscriber<HttpTrade> implements BeanHolderAware {
+public class TradeRelay extends Subscriber<HttpTrade> {
 
     private static final Logger LOG =
             LoggerFactory.getLogger(TradeRelay.class);
@@ -43,12 +39,7 @@ public class TradeRelay extends Subscriber<HttpTrade> implements BeanHolderAware
     public TradeRelay(final TradeReactor reactor) {
         this._tradeReactor = reactor;
     }
-    
-    @Override
-    public void setBeanHolder(final BeanHolder beanHolder) {
-        this._beanHolder = beanHolder;
-    }
-    
+
     @Override
     public void onCompleted() {
         LOG.warn("TradeRelay {} onCompleted", this);
@@ -56,19 +47,12 @@ public class TradeRelay extends Subscriber<HttpTrade> implements BeanHolderAware
 
     @Override
     public void onError(final Throwable e) {
-        LOG.warn("TradeRelay {} onError, detail:{}", 
+        LOG.warn("TradeRelay {} onError, detail:{}",
                 this, ExceptionUtils.exception2detail(e));
     }
 
     @Override
     public void onNext(final HttpTrade trade) {
-        // disable default read policy
-//        if (null != this._beanHolder) {
-//            final ReadPolicy policy = _beanHolder.getBean(ReadPolicy.class);
-//            if (null != policy) {
-//                trade.setReadPolicy(policy);
-//            }
-//        }
         final StopWatch watch4Result = new StopWatch();
         final ReactContext ctx = new ReactContext() {
             @Override
@@ -92,22 +76,15 @@ public class TradeRelay extends Subscriber<HttpTrade> implements BeanHolderAware
             public Observable<? extends DisposableWrapper<HttpObject>> outbound() {
                 return null;
             }
-        }).retryWhen(retryPolicy(trade)).subscribe(new Action1<InOut>() {
-            @Override
-            public void call(final InOut io) {
+        }).retryWhen(retryPolicy(trade)).subscribe(io -> {
                 if (null == io || null == io.outbound()) {
                     LOG.warn("NO_INOUT for trade({}), react io detail: {}.", trade, io);
                 }
-//                trade.writeCtrl().sended().subscribe(sended -> DisposableWrapperUtil.dispose(sended));
                 trade.outbound(buildResponse(trade, trade.inbound(), io));
-            }
-        }, new Action1<Throwable>() {
-            @Override
-            public void call(final Throwable error) {
+            }, error -> {
                 LOG.warn("Trade {} react with error, detail:{}", trade, ExceptionUtils.exception2detail(error));
                 trade.close();
-            }
-        });
+            });
     }
 
     private Observable<? extends DisposableWrapper<HttpObject>> buildResponse(
@@ -115,52 +92,34 @@ public class TradeRelay extends Subscriber<HttpTrade> implements BeanHolderAware
         return (null != io && null != io.outbound())
             ? io.outbound()
             : originalInbound.map(DisposableWrapperUtil.unwrap()).compose(RxNettys.asHttpRequest())
-                .map(new Func1<HttpRequest, HttpVersion>() {
-                    @Override
-                    public HttpVersion call(final HttpRequest req) {
-                        return null != req ? req.protocolVersion() : HttpVersion.HTTP_1_1;
-                    }})
-                .flatMap(new Func1<HttpVersion, Observable<HttpObject>>() {
-                    @Override
-                    public Observable<HttpObject> call(
-                            final HttpVersion version) {
-                        return RxNettys.response200OK(version);
-                    }})
+                .map(req -> null != req ? req.protocolVersion() : HttpVersion.HTTP_1_1)
+                .flatMap(version -> RxNettys.response200OK(version) )
                 .map(DisposableWrapperUtil.wrap(RxNettys.disposerOf(), trade))
                 .delaySubscription(originalInbound.ignoreElements());// response when request send completed
     }
 
+    @SuppressWarnings("unchecked")
     private Func1<Observable<? extends Throwable>, ? extends Observable<?>> retryPolicy(final HttpTrade trade) {
         final RetryPolicy<Integer> policy = new RetryPolicy<Integer>() {
-            @SuppressWarnings("unchecked")
             @Override
             public Observable<Integer> call(final Observable<Throwable> errors) {
                 return (Observable<Integer>) errors.compose(RxObservables.retryIfMatch(ifMatch(trade), 100))
                         .compose(RxObservables.retryMaxTimes(_maxRetryTimes))
                         .compose(RxObservables.retryDelayTo(_retryIntervalBase))
-                        .doOnNext(new Action1<Integer>() {
-                            @Override
-                            public void call(final Integer retryCount) {
-                                LOG.info("FORWARD_RETRY with retry-count {} for trade {}", retryCount, trade);
-                            }})
+                        .doOnNext( retryCount ->
+                            LOG.info("FORWARD_RETRY with retry-count {} for trade {}", retryCount, trade))
                         ;
             }};
-        return new Func1<Observable<? extends Throwable>, Observable<?>>() {
-            @SuppressWarnings("unchecked")
-            @Override
-            public Observable<?> call(final Observable<? extends Throwable> errors) {
-                return policy.call((Observable<Throwable>)errors);
-            }
-        };
+        return errors -> policy.call((Observable<Throwable>)errors);
     }
-    
+
     private static Func1<Throwable, Boolean> ifMatch(final HttpTrade trade) {
         return new Func1<Throwable, Boolean>() {
             @Override
             public Boolean call(final Throwable e) {
                 //  TODO, check obsrequest has been disposed ?
 //                if (trade.inboundHolder().isFragmented()) {
-//                    LOG.warn("NOT_RETRY for trade({}), bcs of trade's inbound has fragmented.", 
+//                    LOG.warn("NOT_RETRY for trade({}), bcs of trade's inbound has fragmented.",
 //                            trade);
 //                    return false;
 //                }
@@ -168,10 +127,10 @@ public class TradeRelay extends Subscriber<HttpTrade> implements BeanHolderAware
                     || (e instanceof ConnectException)
                     || (e instanceof ClosedChannelException);
                 if (matched) {
-                    LOG.info("RETRY for trade({}), bcs of error: {}", 
+                    LOG.info("RETRY for trade({}), bcs of error: {}",
                             trade, ExceptionUtils.exception2detail(e));
                 } else {
-                    LOG.warn("NOT_RETRY for trade({}), bcs of error: {}", 
+                    LOG.warn("NOT_RETRY for trade({}), bcs of error: {}",
                             trade, ExceptionUtils.exception2detail(e));
                 }
                 return matched;
@@ -184,7 +143,6 @@ public class TradeRelay extends Subscriber<HttpTrade> implements BeanHolderAware
     }
 
     private final TradeReactor _tradeReactor;
-    private int _maxRetryTimes = 3;
-    private int _retryIntervalBase = 2;
-    private BeanHolder _beanHolder;
+    private final int _maxRetryTimes = 3;
+    private final int _retryIntervalBase = 2;
 }
