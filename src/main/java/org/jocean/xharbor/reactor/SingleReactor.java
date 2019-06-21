1532d9ebc86909dcefaba2cce380751126d76257
ming.ma <isdom@qq.com>
2019-06-21 21:34:36 +0800

    adjust SingleReactor visible level to public

--- src/main/java/org/jocean/xharbor/reactor/SingleReactor.java
+++ src/main/java/org/jocean/xharbor/reactor/SingleReactor.java
@@ -2,7 +2,7 @@ package org.jocean.xharbor.reactor;
 
 import org.jocean.xharbor.api.TradeReactor;
 
-abstract class SingleReactor implements TradeReactor {
+public abstract class SingleReactor implements TradeReactor {
 
     @Override
     public String[] reactItems() {
